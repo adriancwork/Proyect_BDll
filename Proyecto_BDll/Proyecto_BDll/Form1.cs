e93d5606d4f02535299d94374831c6c74f305e8c
Adrian Figueroa <adrian_cwork@outlook.com>
2019-04-16 14:15:33 -0600

    Conexion establecida.

--- Proyecto_BDll/Proyecto_BDll/Form1.cs
+++ Proyecto_BDll/Proyecto_BDll/Form1.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Proyecto_BDll
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlcnn;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = null;
+            connectionString = "Data Source = lenovo-pc\\sqlserverexpress; Initial Catalog=Muebleria; Integrated Security = true";
+
+            sqlcnn = new SqlConnection(connectionString);
+
+            try
+            {
+                sqlcnn.Open();
+                //MessageBox.Show("Conexión abierta!");
+            }
+
+            catch (Exception ex)
+            {
+                //MessageBox.Show("No se realizó la conexión !");
+                this.Close();
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sqlcnn.Close();
         }
     }
 }
